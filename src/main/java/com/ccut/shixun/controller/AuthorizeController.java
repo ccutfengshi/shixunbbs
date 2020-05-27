8e4c6805dfeb8d4dffa93aeb12fc846c6c9f765e
ccutfengshi <2524734034@qq.com>
2020-05-27 18:42:20 +0800

    使用github关联登录 第三步：配置文件的分离
    出现问题：当redirecturi通过配置文件给值的时候，会报错Connection reset
    解决状态：未解决该问题
    
    注意：当解决该问题的时候，会在日志中标识并给出解决方法

--- src/main/java/com/ccut/shixun/controller/AuthorizeController.java
+++ src/main/java/com/ccut/shixun/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.ccut.shixun.dto.AccessTokenDTO;
 import com.ccut.shixun.dto.GithubUser;
 import com.ccut.shixun.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,24 @@ public class AuthorizeController {
     @Autowired  //自动把spring容器里写好的实例化的实例加载到要使用的上下文中
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")  //配置文件里读key是github.client.id的value，把它赋值到client_id
+    private String clientId;
+
+   @Value("${github.client.secret}")   //配置文件里读key是github.client_secret的value，把它赋值到client_secret
+    private String clientSecret;
+
+   /* @Value("${github.redirect.uri}") //配置文件里读key是github..redirect_uri的value，把它赋值到redirect_uri
+    private String redirectUri;*/
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,  //请求参数接收网站的code
                            @RequestParam(name = "state") String state){  //请求参数接收网站的state
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.cf2d2333aba86f6d");
-        accessTokenDTO.setClient_secret("c535dc2853044b58240b45b803abc1f6b2e06625");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        //accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
